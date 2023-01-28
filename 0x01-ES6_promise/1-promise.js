cd49e83c209f4ffeb1d78738a023fe7be842de48
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2023-01-27 16:13:18 -0800

    Update 1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,12 +1,15 @@
-export default function getFullResponseFromAPI(success) {
+// Promise fake
+function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    if (success === true) {
+    if (success) {
       resolve({
         status: 200,
         body: 'Success',
       });
     } else {
-      reject(Error('The fake API is not working currently'));
+      reject(new Error('The fake API is not working currently'));
     }
   });
 }
+
+export default getFullResponseFromAPI;
