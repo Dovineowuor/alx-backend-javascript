f9b3df3410ce0e4a301e759878bb86d2b9749bbb
Dovine Owuor <70941806+Dovineowuor@users.noreply.github.com>
2023-03-24 13:10:40 +0300

    utilities

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,19 @@
+const Utils = {
+  calculateNumber(type, a, b) {
+    if (type === 'SUM') {
+      return Math.round(a) + Math.round(b);
+    }
+    if (type === 'SUBTRACT') {
+      return Math.round(a) - Math.round(b);
+    }
+    if (type === 'DIVIDE') {
+      if (Math.round(b) === 0) {
+        return 'Error';
+      }
+      return Math.round(a) / Math.round(b);
+    }
+    throw new Error('Error: invalid operation');
+  },
+};
+
+module.exports = Utils;
